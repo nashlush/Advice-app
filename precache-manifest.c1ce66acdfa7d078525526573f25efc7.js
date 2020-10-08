3a231d5e66ee5a227aa9ac2a2d72dd43f49121c6
Avinash Jha <javinash087@gmail.com>
2020-10-08 20:58:49 +0530

    Updates

---
+++ precache-manifest.c1ce66acdfa7d078525526573f25efc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a258f8bc319ea848726306471f2896c3",
+    "url": "/advice-app/index.html"
+  },
+  {
+    "revision": "d4348a72a75f05492c27",
+    "url": "/advice-app/static/css/main.bbb60874.chunk.css"
+  },
+  {
+    "revision": "bf5739fa61359d3fcaa1",
+    "url": "/advice-app/static/js/2.df585b82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice-app/static/js/2.df585b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4348a72a75f05492c27",
+    "url": "/advice-app/static/js/main.f5b6c2dd.chunk.js"
+  },
+  {
+    "revision": "76cf23229ea07ceffd88",
+    "url": "/advice-app/static/js/runtime-main.e9b8dcbb.js"
+  },
+  {
+    "revision": "4108f6a9af4265e248f34e7807b2ccb2",
+    "url": "/advice-app/static/media/city.4108f6a9.jpg"
+  }
+]);
